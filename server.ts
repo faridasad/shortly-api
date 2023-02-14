5fb29c18283334344ad0e6bd014c4e3e8e9026cd
faridasad <asadlifaridd@gmail.com>
2023-02-14 15:49:30 +0400

    Update server

--- server.ts
+++ server.ts
@@ -2,17 +2,21 @@ import { config } from "dotenv";
 config();
 import express from "express";
 import UrlRouter from "./routes/UrlRoutes";
-import { connectDB } from "./db";
+import ShortUrlRouter from "./routes/ShortUrlRoutes";
+import { connectDB } from "./utils/connectDB";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/url", UrlRouter);
+app.use("/", ShortUrlRouter);
 
-connectDB(process.env.MONGO_URI!);
+connectDB(process.env.MONGO_URI!).then(() => {
+  console.log("Connected to DB");
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect("Hi Mom!");
 });
 
 app.listen({ port: parseInt(process.env.PORT!) }, () => {
